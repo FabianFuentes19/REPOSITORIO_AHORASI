74f6c82617774ec0415b75f58d3d101aed00d270
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-06-04 08:33:37 -0600

    se agrego en el setter de nonbre usuario u0na validacion, si el nombre del usuario es vacio o nulo, mostrara un mensaje de advertencia. En el setter de password, validara si la contrasena cuenta con almenos 8 caracteres ademas de validar si cuenta con almenos una letra mayuscula, minuscula y un numero

--- U1_T1_06_ConstrasenaSegura.java/src/UsuarioSeguro.java
+++ U1_T1_06_ConstrasenaSegura.java/src/UsuarioSeguro.java
@@ -10,14 +10,31 @@ public class UsuarioSeguro {
 
     public void setNombreUsuario(String nombreUsuario) {
         this.nombreUsuario = nombreUsuario;
+        if(nombreUsuario == null || nombreUsuario.isEmpty()){
+
+            System.out.println("el nombre del usuario no puede ser nulo y/o no puede estar vacio");
+
+        }
     }
     
     public string getPassword(){
         return password;
     }
 
-    public void setPassword(string password){
-        this.password = password;
+    public string setPassword(string password){
+
+
+        if(password.lenght < 8){
+
+            System.out.println("la contrasena debe tener al menos 8 caracteres");
+
+        }
+        if(!(password.UpperCase() && password.UpperCase() && password.Digit())){
+ 
+            System.out.println("la contrasena debe contener una mayuscula, una minuscula y un numero");
+
+        }
+        return this.password = password;
 
     }
 
