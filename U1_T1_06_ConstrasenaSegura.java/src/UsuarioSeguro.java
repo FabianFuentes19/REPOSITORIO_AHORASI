0606673ccfdf2f714d4ac9d357d80dd46416d8ba
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-06-04 07:56:17 -0600

    se creo el proyecto, ademas de crearse una clase llamada UsuarioSeguro. Dentro de la clase crearon las varibles privadas ademas de crearse tambien los getters y setters

---
+++ U1_T1_06_ConstrasenaSegura.java/src/UsuarioSeguro.java
@@ -0,0 +1,28 @@
+public class UsuarioSeguro {
+
+    
+    private String nombreUsuario;
+    private  string password;
+
+    public String getNombreUsuario(){
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+    
+    public string getPassword(){
+        return password;
+    }
+
+    public void setPassword(string password){
+        this.password = password;
+
+    }
+
+
+
+    
+    
+}
