823ab29a3f56c2b25536313f3fba2e212c6a1188
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-06-13 08:58:26 -0600

    se creo el proyecto, se crearon  3 text field para que el usuario ingrese sus datos personales, tambien un combo box para que el usuario ingrese su rol, ademas de acomodarlos para que tengan un poco de orden

--- U2_TI_05/src/main/java/module-info.java
+++ U2_TI_05/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module mx.edu.utez.u2_ti_05 {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens mx.edu.utez.u2_ti_05 to javafx.fxml;
     exports mx.edu.utez.u2_ti_05;
--- U2_TI_05/src/main/java/mx/edu/utez/u2_ti_05/HelloApplication.java
+++ U2_TI_05/src/main/java/mx/edu/utez/u2_ti_05/HelloApplication.java
@@ -3,16 +3,53 @@ package mx.edu.utez.u2_ti_05;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+
+        TextField nombre = new TextField();
+        TextField apellido = new TextField();
+        TextField edad = new TextField();
+
+        ComboBox<String> comboBox = new ComboBox<>();
+        comboBox.getItems().addAll("estudiante", "profesor", "admin");
+        Label resultado = new Label();
+
+        Button btncrear = new Button("Crear");
+        Button btnreset = new Button("reset");
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.add(new Label("Nombre: "),0,0);
+        grid.add(nombre, 1, 0);
+        grid.add(new Label("Apellido: "),0,1);
+        grid.add(apellido, 1, 1);
+        grid.add(new Label("Edad: "),0,2);
+        grid.add(edad, 1, 2);
+        grid.add(new Label("Rol: "),0,3);
+        grid.add(comboBox, 1, 3);
+        grid.add(btncrear, 1, 4);
+        grid.add(btnreset, 2, 4);
+
+
+
+
+
+        Scene scene = new Scene(grid, 500, 240);
+        stage.setTitle("Formulario T_05");
         stage.setScene(scene);
         stage.show();
     }
