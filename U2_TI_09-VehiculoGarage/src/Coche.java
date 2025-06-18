ef4bcaee082fd417f480dbc8b21919d1761183a9
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-06-18 08:01:23 -0600

    se creo la clase hija coche, la cual se pasa el contructor de la clase padre vehiculo ademas de anadirsele un nuevo atributo propio de la clase nueva, tambien sus getters y setters

--- U2_TI_09-VehiculoGarage/src/Coche.java
+++ U2_TI_09-VehiculoGarage/src/Coche.java
@@ -1,2 +1,15 @@
-public class Coche {
+public class Coche extends Vehiculo {
+
+        private int numPuertas;
+
+    public Coche(String marca, String placa, int numPuertas) {
+        super(marca, placa);
+        this.numPuertas = numPuertas;
+    }
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
 }
