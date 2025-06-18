f6173f0ed530e2276a00314b45018a54faba36cc
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-06-18 08:07:36 -0600

    se creo la clase hija motocleta, se heredo el contructor de la clase padre vehiculo, ademas de anadirsele un atributo privado llamado tieneCaja, ademas de su getter y setter

--- U2_TI_09-VehiculoGarage/src/Motocicleta.java
+++ U2_TI_09-VehiculoGarage/src/Motocicleta.java
@@ -1,2 +1,16 @@
-public class Motocicleta {
+public class Motocicleta extends Vehiculo {
+
+    private boolean tieneCaja;
+
+    public Motocicleta(String marca, String placa, boolean tieneCaja) {
+        super(marca, placa);
+        this.tieneCaja = tieneCaja;
+
+    }
+    public boolean tieneCaja() {
+        return tieneCaja;
+    }
+    public void setTieneCaja(boolean tieneCaja) {
+        this.tieneCaja = tieneCaja;
+    }
 }
